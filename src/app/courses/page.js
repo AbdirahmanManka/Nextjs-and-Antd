f41fda3682bb63ed5d7bd4e5d9fb685df2b6f4e2
Abdirahman <114359001+OmarAbdirahman@users.noreply.github.com>
2024-07-17 11:50:59 +0300

    new changes to courses page

--- src/app/courses/page.js
+++ src/app/courses/page.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
 function page() {
-  return <div><h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+  return <div><h1 className="text-3xl text-green font-bold underline ">Hello, Next.js!</h1>
+  <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-green-500">Welcome to courses page</h1>
+    </div>
 </div>
 
 }
